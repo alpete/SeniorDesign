96cb9a0d4ac549248dd3c205013089e99fec9908
alpete <alpete@ksu.edu>
2023-04-03 21:52:24 -0500

    update

--- SeniorDesign/MainWindow.xaml.cs
+++ SeniorDesign/MainWindow.xaml.cs
@@ -24,5 +24,62 @@ namespace SeniorDesign
         {
             InitializeComponent();
         }
+
+        public void SetSupplyVoltage(object sender, RoutedEventArgs e)
+        {
+            Window window = new Window
+            {
+                Title="Set Supply Voltage",
+                Height = 250, Width = 400,
+                Content = new SetValuePopup(1)
+            };
+            window.ShowDialog();
+        }
+
+        public void SetCurrentLimit(object sender, RoutedEventArgs e)
+        {
+            Window window = new Window
+            {
+                Title = "Set Current Limit",
+                Height = 250, Width = 400,
+                Content = new SetValuePopup(2)
+            };
+            window.ShowDialog();
+        }
+
+        public void SetBrakeConstantCurrent(object sender, RoutedEventArgs e)
+        {
+            Window window = new Window
+            {
+                Title = "Set Brake Constant Current",
+                Height = 250, Width = 400,
+                Content = new SetValuePopup(3)
+            };
+            window.ShowDialog();
+        }
+
+        public void SetOpenLoopTorque(object sender, RoutedEventArgs e)
+        {
+            Window window = new Window
+            {
+                Title = "Set Open Loop Torque",
+                Height = 250, Width = 400,
+                Content = new SetValuePopup(4)
+            };
+            window.ShowDialog();
+        }
+
+        public void SetClosedLoopTorque(object sender, RoutedEventArgs e)
+        {
+            Window window = new Window
+            {
+                Title = "Set Closed Loop Torque",
+                Height = 250, Width = 400,
+                Content = new SetValuePopup(5)
+            };
+            window.ShowDialog();
+        }
+
+
     }
 }
---
+++ SeniorDesign/SetValuePopup.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Linq;
+
+namespace SeniorDesign
+{
+    /// <summary>
+    /// Interaction logic for SetValuePopup.xaml
+    /// </summary>
+    public partial class SetValuePopup : UserControl
+    {
+        int mode; // 1 = set supply voltage, 2 = set current limit, 3 = brake constant current, 4 = open loop torque, 5 = closed loop torque
+
+        public SetValuePopup(int mode)
+        {
+            InitializeComponent();
+            this.mode = mode;
+            foreach (Grid g in FindVisualChildren<Grid>(this))
+            {
+                foreach (TextBlock tb in FindVisualChildren<TextBlock>(g))
+                {
+                    if (tb.Tag.Equals("This"))
+                    {
+                        switch (this.mode)
+                        {
+                            case 1:
+                                tb.Text = "Set Supply Voltage (Volts)";
+                                break;
+                            case 2:
+                                tb.Text = "Set Current Limit (Amps)";
+                                break;
+                            case 3:
+                                tb.Text = "Set Brake Constant Current (Amps)";
+                                break;
+                            case 4:
+                                tb.Text = "Set Open Loop Torque";
+                                break;
+                            case 5:
+                                tb.Text = "Set Closed Loop Torque";
+                                break;
+                        }
+                    }
+                }
+            }
+            
+        }
+
+        public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj == null) yield return (T)Enumerable.Empty<T>();
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+            {
+                DependencyObject ithChild = VisualTreeHelper.GetChild(depObj, i);
+                if (ithChild == null) continue;
+                if (ithChild is T t) yield return t;
+                foreach (T childOfChild in FindVisualChildren<T>(ithChild)) yield return childOfChild;
+            }
+        }
+
+
+    }
+}
